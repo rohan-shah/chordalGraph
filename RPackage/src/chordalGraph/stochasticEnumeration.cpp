ee0998e0d1ed4a7c6c7905711ddc451a964d9533
Rohan Shah <rohan.shah@bookshah.com>
2015-09-21 11:43:57 +1000

    Added text progress bar in R

--- RPackage/src/chordalGraph/stochasticEnumeration.cpp
+++ RPackage/src/chordalGraph/stochasticEnumeration.cpp
@@ -13,13 +13,23 @@ BEGIN_RCPP
 	args.nVertices = nVertices;
 	args.budget = budget;
 
+	int maxEdges = ((nVertices - 1)*nVertices / 2);
+
+	//Set up text progress bar
+	Rcpp::Function txtProgressBar("txtProgressBar");
+	Rcpp::Function close("close");
+	Rcpp::Function setTxtProgressBar("setTxtProgressBar");
+	Rcpp::RObject barHandle = txtProgressBar(Rcpp::Named("style") = 3, Rcpp::Named("min") = 0, Rcpp::Named("max") = maxEdges, Rcpp::Named("initial") = 0);
+
 	std::vector<std::string> estimatesAsStrings;
-	for (int nEdges = 0; nEdges < ((nVertices-1)*nVertices/2)+1; nEdges++)
+	for (int nEdges = 0; nEdges < maxEdges+1; nEdges++)
 	{
+		setTxtProgressBar(barHandle, nEdges);
 		args.nEdges = nEdges;
 		chordalGraph::stochasticEnumeration(args);
 		estimatesAsStrings.push_back(args.estimate.str());
 	}
+	close(barHandle);
 	SEXP estimatesAsStrings_sexp = Rcpp::wrap(estimatesAsStrings);
 
 	Rcpp::Function mpfrFunction("mpfr");
