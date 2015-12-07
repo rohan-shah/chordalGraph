b4dec70c20e8c04a1d1cb9687e32c2107b8f791e
Rohan Shah <github@bookshah.com>
2015-12-08 00:56:03 +1000

    Changes to accomodate Rcpp changes. Added first tests.

--- RPackage/src/chordalGraph/register.cpp
+++ RPackage/src/chordalGraph/register.cpp
@@ -1,4 +1,5 @@
 #include <Rcpp.h>
+#include <internal.h>
 #include "isChordalIterative.h"
 #ifdef _MSC_VER
 	#undef RcppExport
@@ -24,9 +25,24 @@ R_CallMethodDef callMethods[] =
 	{"horvitzThompsonSpecificEdges", (DL_FUNC)&horvitzThompsonSpecificEdges, 5},
 	{NULL, NULL, 0}
 };
-extern "C" void R_init_Rcpp(DllInfo* info);
 RcppExport void R_init_chordalGraph(DllInfo *info)
 {
-	R_init_Rcpp(info);
-	R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+	std::vector<R_CallMethodDef> callMethodsVector;
+	R_CallMethodDef* mpMap2CallMethods = callMethods;
+	while(mpMap2CallMethods->name != NULL) mpMap2CallMethods++;
+	callMethodsVector.insert(callMethodsVector.begin(), callMethods, mpMap2CallMethods);
+
+#ifdef CUSTOM_STATIC_RCPP
+	R_CallMethodDef* RcppStartCallMethods = Rcpp_get_call();
+	R_CallMethodDef* RcppEndCallMethods = RcppStartCallMethods;
+	while(RcppEndCallMethods->name != NULL) RcppEndCallMethods++;
+	callMethodsVector.insert(callMethodsVector.end(), RcppStartCallMethods, RcppEndCallMethods);
+#endif
+	R_CallMethodDef blank = {NULL, NULL, 0};
+	callMethodsVector.push_back(blank);
+
+	R_registerRoutines(info, NULL, &(callMethodsVector[0]), NULL, NULL);
+#ifdef CUSTOM_STATIC_RCPP
+	init_Rcpp_cache();
+#endif
 }
