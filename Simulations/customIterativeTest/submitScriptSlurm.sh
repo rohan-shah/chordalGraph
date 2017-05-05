4826822bf899fff6ca057b185cfa248eeff5796c
Rohan Shah <uqrshah@smp-login-0.local>
2017-05-05 16:31:01 +1000

    Removed un-needed simulation scripts.

--- Simulations/customIterativeTest/submitScriptSlurm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=30:00:00
-#SBATCH --mem=10GB
-#SBATCH --job-name=chordalGraph
-trap "echo recieved SIGUSR1;" SIGUSR1;
-R CMD BATCH --no-save --no-restore --  script.R script.Rout.$SLURM_JOBID
